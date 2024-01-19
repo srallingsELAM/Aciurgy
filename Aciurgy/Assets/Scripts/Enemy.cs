5a124b43a525b6238be31229870b239d95ccbe4a
srallingsELAM <srallings2023@elam.co.uk>
2024-01-19 12:46:20 +0000

    Push

--- Aciurgy/Assets/Scripts/Enemy.cs
+++ Aciurgy/Assets/Scripts/Enemy.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    private Waypoints Wpoints;
+
+    private int waypointIndex;
+
     void Start()
     {
-        
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            if (waypointIndex < Wpoints.waypoints.Length - 1)
+            {
+                waypointIndex++;
+
+            } else {
+
+                waypointIndex = 0;
+            }      
+        }
     }
 }
