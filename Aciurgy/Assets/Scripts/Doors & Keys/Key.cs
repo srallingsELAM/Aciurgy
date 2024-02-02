ae67ea56356623be2c8625e0f8ec47f06b5b5e7f
srallingsELAM <srallings2023@elam.co.uk>
2024-02-02 12:45:39 +0000

    push

--- Aciurgy/Assets/Scripts/Doors & Keys/Key.cs
+++ Aciurgy/Assets/Scripts/Doors & Keys/Key.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour
+
+    
 {
+
+    public float time;
     public GameObject door;
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -11,9 +15,15 @@ public class Key : MonoBehaviour
         if (col.CompareTag("Player"))
         {
             door.GetComponent<BoxCollider2D>().enabled = false;
-          
+            
+
         }
-       
-    
+        if (col.CompareTag("Door"))
+        {
+            
+            Destroy(gameObject, time);
+
+        }
+
     }   
 }
