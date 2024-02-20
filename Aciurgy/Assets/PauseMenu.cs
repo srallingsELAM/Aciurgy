dad8c55d2dd135b0f27f3da7ce536bde5182f24c
srallingsELAM <srallings2023@elam.co.uk>
2024-02-20 12:48:31 +0000

    push pause menu

--- Aciurgy/Assets/PauseMenu.cs
+++ Aciurgy/Assets/PauseMenu.cs
@@ -6,20 +6,35 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject ControlsMenu;
     public void  Pause()
     {
        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
     }
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+    public void Controls()
+    {
+        ControlsMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Resume1()
+    {
+        ControlsMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
